cf8c3c815046afc5bf8c4e99c0aca8942811b62a
Ivan Blancarte <iblancarte583@gmail.com>
2025-06-13 07:57:43 -0600

    feat: actualiación

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,41 @@
-import { Button, StyleSheet, Text, View } from "react-native";
 import LoginPage from "@/src/screens/auth/LoginPage";
-import { AuthProvider } from "@/src/context/AuthContext";
-import LayoutCR from "./encargadoCR/_layout";
+import * as Updates from "expo-updates";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import "../global.css";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // 1) Comprueba si hay update
+        const { isAvailable } = await Updates.checkForUpdateAsync();
+        if (isAvailable) {
+          // 2) Descárgalo
+          await Updates.fetchUpdateAsync();
+          // 3) Recarga la app con la nueva versión
+          await Updates.reloadAsync();
+          return; // nunca llegamos a montar la UI vieja
+        }
+      } catch (e) {
+        console.error("Error en expo-updates:", e);
+      } finally {
+        // Si no hay update o falló, marcamos ready para renderizar la UI
+        setIsReady(true);
+      }
+    })();
+  }, []);
+
+  // Mientras chequea/descarga el update, mostramos un loader
+  if (!isReady) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <>
       <View style={{ flex: 1 }}>
